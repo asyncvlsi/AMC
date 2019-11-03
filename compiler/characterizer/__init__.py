84ace45ffc36a759f311d340c90411055ce69e70
samira <fa292@c34n09.grace.hpc.yale.internal>
2019-11-03 15:47:43 -0500

    Minor modifications.

--- compiler/characterizer/__init__.py
+++ compiler/characterizer/__init__.py
@@ -1,3 +1,10 @@
+# BSD 3-Clause License (See LICENSE.OR for licensing information)
+# Copyright (c) 2016-2019 Regents of the University of California 
+# and The Board of Regents for the Oklahoma Agricultural and 
+# Mechanical College (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+
+
 import os
 import debug
 from globals import OPTS,find_exe,get_tool
@@ -13,6 +20,10 @@ if OPTS.spice_name != "":
         debug.error("{0} not found. Unable to perform characterization.".format(OPTS.spice_name),1)
 else:
     (OPTS.spice_name, OPTS.spice_exe) = get_tool("spice",["hsim", "vcs"])
+
+# set the input dir for spice files if using ngspice 
+if OPTS.spice_name == "ngspice":
+    os.environ["NGSPICE_INPUT_DIR"] = "{0}".format(OPTS.AMC_temp)
     
 if OPTS.spice_exe == "":
     debug.error("No recognizable spice version found. Unable to perform characterization.",1)
